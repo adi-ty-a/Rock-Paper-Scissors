bafd23416e4d1f4f5e07dccc79c050275c4a069e
adi-ty-a <adi.personal.13@gmail.com>
2024-04-14 17:33:40 +0530

    5 turn and modified

--- MyScript.js
+++ MyScript.js
@@ -1,3 +1,4 @@
+let computerscore=0,playerscore=0;
 function getComputerChoice(){
     let num = Math.floor((Math.random()*3)+1);
     if(num == 1){
@@ -10,30 +11,63 @@ function getComputerChoice(){
         return "SCISSOR";
     }
 }
-console.log(getComputerChoice());
 function playerSelection(){
     let choice = prompt("enter your choise 'Rock','Paper,'Scissor'");
     return choice.toUpperCase();
 }
 function compare(computer, player){
     if(computer == "ROCK" && player == "PAPER"){
+        playerscore++;
         return "You WIN! Paper beats Rock";
+        
     }
     else if(computer == "PAPER" && player == "ROCK"){
+        computerscore++;
         return "You LOSE! Paper beats Rock";
+        
     }
     else if(computer == "SCISSOR" && player == "PAPER"){
+        computerscore++;
         return "You LOSE! scissor beats paper";
+        
     }
     else if(computer == "SCISSOR" && player == "ROCK"){
+        playerscore++;
         return "You WIN! rock beats scissor";
+        
     }
     else if(computer == "PAPER" && player == "SCISSOR"){
+        playerscore++;
         return "You WIN! scissor beats paper";
+        
     }
     else if(computer == "ROCK" && player == "SCISSOR"){
+        computerscore++;
         return "You LOSE! rock beats scissor";
+        
+    }
+    else if(computer == "ROCK" && player == "ROCK"){
+        return "It's a tie";
+    }
+    else if(computer == "PAPER" && player == "PAPER"){
+        return "It's a tie";
+    }
+    else if(computer == "SCISSOR" && player == "SCISSOR"){
+        return "It's a tie";
     }
 }
-let result = compare(getComputerChoice(),playerSelection());
-window.alert(result);
+function playGame(){
+for(let i=1;i<=5;i++){
+    console.log(computerscore);
+    console.log(playerscore);
+    compare(getComputerChoice(),playerSelection());
+    console.log(getComputerChoice());
+}
+    if(computerscore > playerscore){
+        window.alert("computer win");
+    }
+    else{
+        window.alert("player win");
+    }
+}
+playGame();
